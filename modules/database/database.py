2b433adfcec742e24499dafc254410d1b60115a0
alberto <sleeps.doyens0y@icloud.com>
2026-02-26 14:59:06 -0600

    Created several database functions

--- modules/database/database.py
+++ modules/database/database.py
@@ -6,10 +6,14 @@ import csv
 import modules.constants.main as const
 from modules.csv_tools.main import read_file_pandas, fix_columns_to_match_db, fix_data_before_insert_to_db
 import os
+import sys
 import pandas as pd
 import ast
 import json
 from datetime import datetime
+import traceback
+import uuid
+import sys
 
 # CHECK: other_emails should be added as a list
 
@@ -29,6 +33,7 @@ def insert_new_recruits(file_path) -> None:
     """
     Inserts all records found in a csv file into the recruits database
     """
+    batch_id = str(uuid.uuid4())
     conn = sqlite3.connect(const.database_path)
     cursor = conn.cursor()
     cursor.execute("PRAGMA foreign_keys = ON;")
@@ -36,16 +41,26 @@ def insert_new_recruits(file_path) -> None:
         reader = csv.reader(file)
         headers = next(reader)
         headers_str = ', '.join(headers)
-        placeholders = ', '.join(['?'] * len(headers))
-        query = 'INSERT OR IGNORE INTO recruits ({0}) VALUES ({1})'.format(headers_str, placeholders)
+        placeholders = ', '.join(['?'] * (len(headers)+1)) # CHECK: the +1 is because batch id is added after in the query
+        query = 'INSERT OR IGNORE INTO recruits ({0}, batch_id) VALUES ({1});'.format(headers_str, placeholders)
         rows_to_insert = []
         for row in reader:
             row = [None if value == "" else value for value in row]
+            row.append(batch_id)
             rows_to_insert.append(row)
-
+    
     cursor.executemany(query, rows_to_insert)
     conn.commit()
+
+    cursor.execute(
+        "SELECT id FROM recruits WHERE batch_id = ?;",
+        (batch_id,)
+    )
+
+    new_ids = cursor.fetchall()
     conn.close()
+
+    return new_ids
 
 def insert_update_recruits(file_path) -> None:
     """
@@ -183,19 +198,34 @@ def get_update_records(list_of_emails:list) -> pd.DataFrame:
 #     insert_new_recruits(new_emails_path)
 #     os.remove(new_emails_path)
 
-def insert_new_csv_to_db(file_path, source, status, project_id):
+def prepare_csv_for_database_input(file_path, source, status, project_id):
+    try:
+        df = read_file_pandas(file_path)
+        df = fix_columns_to_match_db(df, file_path, source, status, project_id)
+        df = fix_data_before_insert_to_db(df)
+        df.to_csv(const.DATABASE_INPUT_DIR / Path(file_path).name, index=False)
+    except Exception as e:
+        print('failed preparing csv for database input. Error: ',e)
+    # else:
+    #     os.remove(file_path)
+
+def insert_new_csv_to_db(file_path):
     """
     Processes files into the database
     it saves new recruits and updates recruits
     """
     df = read_file_pandas(file_path)
-    df = fix_columns_to_match_db(df, file_path, source, status, project_id)
-    df = fix_data_before_insert_to_db(df)
+    project_id = df.loc[0,'projects_ids']
+    # project_id = project_id[2:-2]
+    # print(project_id)
+    # sys.exit()
 
     # Getting duplicate emails
     list_of_emails = list(df.email)
     update_records = get_update_records(list_of_emails)
     emails = list(update_records.email)
+    ids_from_old_recruits = list(update_records.ID)
+    ids_from_old_recruits = [(x,) for x in ids_from_old_recruits]
 
     ########################
     # Saving new emails
@@ -203,7 +233,8 @@ def insert_new_csv_to_db(file_path, source, status, project_id):
     new_emails_path = const.TEMP_DB_DIR.joinpath(file_path.name)
     df_new_recruits.to_csv(new_emails_path, index=False)
     print('saving new records...')
-    insert_new_recruits(new_emails_path)
+
+    new_ids = insert_new_recruits(new_emails_path)
     q_new_recruits = len(df_new_recruits)
     print('inserted {0} new records'.format(q_new_recruits))
     os.remove(new_emails_path)
@@ -219,6 +250,12 @@ def insert_new_csv_to_db(file_path, source, status, project_id):
     print('updating {0} records'.format(q_update_recruits))
     bulk_update_records(update_emails_path)
     os.remove(update_emails_path)
+
+    new_ids.extend(ids_from_old_recruits)
+
+    os.remove(file_path)
+
+    return new_ids, project_id
 
 # CHECK: seems like the following two are the same?
 def bulk_update_records(table_name:str):
@@ -407,7 +444,7 @@ def add_new_million_verifier_job(data:json, project_id:str) -> None:
 
 def get_processing_jobs():
     conn, cursor = connect_to_db()
-    cursor.execute("SELECT job_id FROM million_verifier_jobs WHERE status='processing'")
+    cursor.execute("SELECT job_id FROM million_verifier_jobs WHERE status<>'completed'")
     return cursor.fetchall()
 
 def update_mv_job_status(job_id, status, output_path=None):
@@ -419,9 +456,231 @@ def update_mv_job_status(job_id, status, output_path=None):
     """, (status, datetime.utcnow(), output_path, job_id))
     conn.commit()
     
+def save_records_to_project(project_id:str):
+    try:
+        conn, cursor = connect_to_db()
+        # sql_file_path = const.projects_base_path / project_id / '{}.sql'.format(project_id)
+        sql_file_path = '/Users/albertoruizcajiga/python/sis_international/modules/database/files/temp/test_sql.sql'
+        with open(sql_file_path, 'r') as file:
+            query = file.read()
+
+        cursor.execute(query)
+        results = cursor.fetchall()
+
+        values = []
+        project_id_int = int(project_id)
+        for x in results:
+            values.append((project_id_int, x[0]))
+
+        
+        cursor.executemany("""
+            INSERT OR IGNORE INTO project_recruits (project_id, recruit_id)
+            VALUES (?, ?)
+        """, values)
+
+        conn.commit()
+        conn.close()
+        print('success!')
+    except Exception as e:
+        print('Something failed saving records for a project. Error: ', e)
+        traceback.print_exc()
+
+def save_newly_added_records_to_project(project_id:str, ids:list[tuple]) -> None:
+    try:
+        values = []
+        project_id_int = int(project_id)
+        for x in ids:
+            values.append((project_id_int, x[0]))
+
+        conn, cursor = connect_to_db()
+        cursor.executemany("""
+            INSERT OR IGNORE INTO project_recruits (project_id, recruit_id)
+            VALUES (?, ?)
+        """, values)
+
+        conn.commit()
+        conn.close()
+        print('success!')
+    except Exception as e:
+        print('Something failed saving records for a project. Error: ', e)
+        traceback.print_exc()
+
+def get_project_recruits(project_id:str):
+    conn, cursor = connect_to_db()
+    cursor.execute("""
+        SELECT r.*
+        FROM recruits AS r
+        JOIN project_recruits AS pr 
+            ON r.ID = pr.recruit_id
+        WHERE pr.project_id = ?;
+    """, (project_id,))
+    return cursor.fetchall()
+
+def get_ss_ready_records(project_id:str, limit:str):
+    """
+    ss ready meaning valid records
+    AND
+    not sent in a period of time
+    """
+    conn, cursor = connect_to_db()
+
+    cursor.execute("""
+        SELECT r.ID, r.first_name, r.email
+        FROM recruits as r
+        JOIN
+            project_recruits AS pr
+        ON
+            r.ID = pr.recruit_id
+        WHERE pr.project_id = ?
+        AND
+        (pr.last_sent_at < DATE('now', '-30 days') OR
+        pr.last_sent_at IS NULL)
+        AND
+        r.email NOT LIKE "%gmail%"
+        AND
+        r.email_validation IN ('valid', 'ok')
+        AND
+        r.is_active = 1
+        LIMIT ?
+        ;""", (project_id, limit))
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+def get_mv_ready_records(project_id:str, limit:str):
+    """
+    ss ready meaning valid records
+    AND
+    not sent in a period of time
+    """
+    conn, cursor = connect_to_db()
+
+    limit = str(int(int(limit) * 1.1)) # so that the we have less verifying jobs by validating a bit more (counting for invalid results)
+
+    cursor.execute("""
+        SELECT r.ID, r.first_name, r.email
+        FROM recruits as r
+        JOIN
+            project_recruits AS pr
+        ON
+            r.ID = pr.recruit_id
+        WHERE pr.project_id = ?
+        AND
+        (pr.last_sent_at < DATE('now', '-30 days') OR
+        pr.last_sent_at IS NULL)
+        AND
+        (r.email_validation IS NULL OR
+        r.email_validation  = 'not verified')
+        AND
+        r.is_active = 1
+        LIMIT ?
+        ;""", (project_id, limit))
+    return cursor.fetchall()
+
+def insert_new_blasting_quotas(file_path:str):
+    conn, cursor = connect_to_db()
+    with open(file_path, 'r') as file:
+        reader = csv.reader(file)
+        headers = next(reader)
+        headers_str = ', '.join(headers)
+        placeholders = ', '.join(['?'] * len(headers))
+        query = 'INSERT INTO daily_project_quota ({0}) VALUES ({1})'.format(headers_str, placeholders)
+        rows_to_insert = []
+        for row in reader:
+            row = [None if value == "" else value for value in row]
+            rows_to_insert.append(row)
+
+    cursor.executemany(query, rows_to_insert)
+    conn.commit()
+    conn.close()
+
+def get_today_blast_quota():
+    conn, cursor = connect_to_db()
+    query = """SELECT *
+            FROM daily_project_quota
+            WHERE run_date = DATE('now')
+            ;"""
+    cursor.execute(query)
+    return cursor.fetchall()
+
+def get_project_limit(project_id):
+    conn, cursor = connect_to_db()
+    query = """SELECT 
+                    dq.total_records -
+                    COUNT(pr.recruit_id) AS remaining
+                FROM daily_project_quota AS dq
+                LEFT JOIN project_recruits AS pr
+                    ON pr.project_id = dq.project_id
+                    AND pr.last_sent_at >= DATE('now')
+                WHERE
+                dq.project_id = ?
+                AND
+                dq.run_date = DATE('now')
+                GROUP BY dq.total_records;"""      
+    values = (project_id,)
+    cursor.execute(query, values)
+    data = cursor.fetchall()
+    conn.close()
+    return data
+
+def update_project_recruits_last_sent(project_id:str, recruits_ids:list):
+    query = """UPDATE project_recruits
+            SET 
+            last_sent_at = DATE('now')
+            WHERE project_id = ?
+            AND recruit_id = ?
+            ;"""
+    values = []
+    for x in recruits_ids:
+        values.append((project_id, x))
+    conn, cursor = connect_to_db()
+    cursor.executemany(query, values)
+    conn.commit()
+    conn.close()
+
+def get_active_validation_jobs_for_project(project_id:str):
+    conn, cursor = connect_to_db()
+    query = """SELECT COUNT(*)
+                FROM million_verifier_jobs
+                WHERE status <> 'completed'
+                AND
+                project_id = ?;
+                """
+    values = (project_id,)
+    cursor.execute(query, values)
+    results = cursor.fetchone()
+    conn.close()
+    return results
+
+def get_ss_campaign_id(project_id:str):
+    conn, cursor = connect_to_db()
+    query = 'SELECT ss_campaign_id FROM projects WHERE project_id = ?'
+    values = (project_id,)
+    cursor.execute(query, values)
+
+    data = cursor.fetchone()
+    data = data[0]
+
+    conn.close()
+
+    return data
+
+def get_project_name_with_project_number(project_id:str) -> tuple:
+    conn, cursor = connect_to_db()
+    query = """SELECT project_name
+    FROM projects
+    WHERE project_id = ?
+    """
+    values = (project_id,)
+    cursor.execute(query, values)
+    data = cursor.fetchone()
+    conn.close()
+
+    return data
 
 # query to select all matches
 # create table projects_recipients
 # query to save matches to projects_recipients
 # query to extract N elements from recruits considering projects_recipients
 # query to save extractions into mail_events
+
